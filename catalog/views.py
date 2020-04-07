60c6bbe0c07411e64585123e1243743a8370b3e1
Krytosys <47263260+Krytosys@users.noreply.github.com>
2020-04-07 16:11:01 +0800

    For Registration page
    +Has basics signups
    - Question
    - Admin changes

--- catalog/views.py
+++ catalog/views.py
@@ -158,3 +158,28 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('books')
+
+#Registration stuff (7/4/20)
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django.shortcuts import redirect
+
+def signup_view(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        user.refresh_from_db()
+        user.profile.first_name = form.cleaned_data.get('first_name')
+        user.profile.last_name = form.cleaned_data.get('last_name')
+        user.profile.idno = form.cleaned_data.get('idno')
+        user.profile.email = form.cleaned_data.get('email')
+        user.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('/catalog')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form':form})    
